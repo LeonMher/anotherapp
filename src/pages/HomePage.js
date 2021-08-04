fe264d1a6b4aa20b844dd7e599ec91c672cec747
LeonMher <99.p3rc3n7@gmail.com>
2021-08-03 22:38:16 -0500

    here's another try

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 function HomePage() {
     return (
-        <div>
+        <div className='homePage'>
             <h1>Welcome to Alaverdi</h1>
+
+            <div className='cont1'></div>
+            <div className='cont2'></div>
+            <div className='cont3'></div>
+            <div className='cont4'></div>
+            <div className='cont5'></div>
+
         </div>
     )
 }
